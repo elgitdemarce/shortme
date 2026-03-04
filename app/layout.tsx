3e755c9a96687ced8d6be47fae856eb026af2b4b
elgitdemarce <124519731+elgitdemarce@users.noreply.github.com>
2026-03-04 14:43:09 -0300

    Create RootLayout component with metadata

---
+++ app/layout.tsx
@@ -0,0 +1,16 @@
+export const metadata = {
+  title: "Shortme",
+  description: "Simple URL shortener",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
