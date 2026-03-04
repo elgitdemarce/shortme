5d745eb9301147762df56d68325417e13f80140c
elgitdemarce <124519731+elgitdemarce@users.noreply.github.com>
2026-03-04 17:10:51 -0300

    Refactor GET function to use kv for URL retrieval
    Refactor GET function to retrieve original URL from kv store instead of decompressing code.

--- app/[code]/route.ts
+++ app/[code]/route.ts
@@ -1,22 +1,17 @@
 export const runtime = "nodejs";
-import { brotliDecompress } from "zlib";
-import { promisify } from "util";
-import { NextResponse } from "next/server";
 
-const decompress = promisify(brotliDecompress);
+import { kv } from "@vercel/kv";
+import { NextResponse } from "next/server";
 
 export async function GET(
   request: Request,
   { params }: { params: { code: string } }
 ) {
-  try {
-    const buffer = Buffer.from(params.code, "base64url");
-    const decompressed = await decompress(buffer);
+  const originalUrl = await kv.get<string>(params.code);
 
-    const originalUrl = decompressed.toString();
-
-    return NextResponse.redirect(originalUrl);
-  } catch {
-    return new NextResponse("Invalid short URL", { status: 400 });
+  if (!originalUrl) {
+    return new NextResponse("Invalid short URL", { status: 404 });
   }
+
+  return NextResponse.redirect(originalUrl);
 }
